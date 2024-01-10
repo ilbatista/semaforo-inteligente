e54e8a3c166312032ae880b3f837008ab9e3e54b
Ives Lima <ives.lb@gmail.com>
2024-01-09 20:52:59 -0400

    Segunda revisão do projeto.

--- SemaforoInteligente.ino
+++ SemaforoInteligente.ino
@@ -1,75 +1,119 @@
-#include <Adafruit_LiquidCrystal.h>
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(0, 1, 4, 5, 6, 7);
 
-Adafruit_LiquidCrystal lcd(0);
-int tempoAbertura = 10000;
+byte p[2][8] = {
+  {B01100,B01100,B00000,B01110,B01100,B01100,B01100,B01110},
+  {B01100,B01100,B00000,B01110,B11100,B01100,B11010,B10011}
+};
+
 unsigned long timerInterno;
-  
+int posicao = 0;
+byte indice = 0;
+
 void setup(){
   timerInterno = millis();
- 
-  DDRD = 0xFC;
-  DDRB = 0x3E;
+
+  DDRD = 0b11111011;
+  DDRB = 0b111111;
+
+  for(byte i=0;i<2;i++)
+  {
+    lcd.createChar(i,p[i]);
+  }
+
+  lcd.begin(16,2);
   
-  lcd.begin(16, 2);
   lcd.print("Grupo 4");
-  lcd.setCursor(0,1);
+  delay(1000);
+  lcd.clear(); 
   lcd.print("Semaforo");
-  delay(1000);
-  lcd.clear();
-  lcd.print("A: automoveis");
   lcd.setCursor(0,1);
-  lcd.print("P: pedestres");
-  delay(1000);
+  lcd.print("Inteligente");
+  delay(2000);
   lcd.clear();
-  
-  PORTD = 0x30;
-  lcd.print("A: verde");
+
+  tone(3,500,100);
+  PORTB = 0b001100;
+  lcd.print("Passagem fechada");
   lcd.setCursor(0,1);
-  lcd.print("P: vermelho");
+  lcd.print("################");
 }
 
-void loop(){
-  int statusBotao = digitalRead(8);
+void loop(){  
+  int botao = digitalRead(2);
   delay(50);
-  
-  if(statusBotao == HIGH && (millis() - timerInterno) > 5000){
-  	lcd.setCursor(0,0);
-    lcd.clear();
-    alteraLuzes();
+
+  if(botao == HIGH &&(millis() - timerInterno) > 5000){
+  	tone(3,750,100);
+    acionaSemaforo();
   }
 }
-     
-void alteraLuzes(){
-  tone(9,500,100);
-  PORTD = 0x28;
-  lcd.print("A: amarelo");
-  lcd.setCursor(0,1);
-  lcd.print("P: vermelho");
+
+void acionaSemaforo(){
+  PORTB = 0b001010;
+  limpaLinha(0);
+  lcd.home();
+  lcd.print("Aguarde...      ");
   delay(5000);
   lcd.clear();
 
-  tone(9,750,100);
-  PORTD = 0x44;
-  lcd.print("A: vermelho");
-  lcd.setCursor(0,1);
-  lcd.print("P: verde");
-  
-  delay(tempoAbertura);
+  tone(3,1000,100);
+  PORTB = 0b010001;
+  lcd.print("Passagem livre!");
+  posicao = 0;
   
   for (int x=0; x<10; x++){
-    PORTD = 0x4;
-    delay(500);
-    PORTD = 0x44;
-    delay(500);
+    animacao();
+    delay(1000);
   }
   
+  int t = 5;
+  
+  for (int x=0; x<5; x++){
+    lcd.home();
+    lcd.print("Fechando em 0:0");
+    
+    contadorPedestres(t);
+    lcd.print(t);
+    t--;
+    PORTB = 0b010001;
+    animacao();
+    delay(500);
+    PORTB = 0b000001;
+    animacao();
+    delay(500);
+    tone(3,1000,100);
+  }
+
   lcd.clear();
-  
-  tone(9,1000,100);
-  PORTD = 0x30;  
-  lcd.print("A: verde");
+  tone(3,500,100);
+  PORTB = 0b001100;
+  lcd.print("Passagem fechada");
   lcd.setCursor(0,1);
-  lcd.print("P: vermelho");
-  
+  lcd.print("################");
+
   timerInterno = millis();
 }
+
+void limpaLinha(byte linha){
+  lcd.setCursor(0,linha);
+  lcd.print("________________");
+  lcd.setCursor(0,linha);
+}
+
+void contadorPedestres(int cont){
+  lcd.setCursor(15,0);
+}
+
+void animacao(){  
+  limpaLinha(1);
+  lcd.setCursor(posicao,1);
+  lcd.write(indice);
+  indice++;
+  if(indice > 1)
+    indice = 0;
+  posicao++;
+  if(posicao == 16)
+    posicao = 0;
+}
+
