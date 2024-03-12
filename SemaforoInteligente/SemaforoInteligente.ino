86a397fddda53563a631b5311e1136f653df2b79
Ives Lima <ives.lb@gmail.com>
2024-03-12 14:12:54 -0400

    Port do código para ESP32 e implementação da matriz WS2812B

--- SemaforoInteligente/SemaforoInteligente.ino
+++ SemaforoInteligente/SemaforoInteligente.ino
@@ -1,28 +1,26 @@
-#include <LiquidCrystal.h>
+#include "LiquidCrystal_I2C.h"
 #include "Button.h"
 #include "FiniteStateMachine.h"
+#include "Adafruit_NeoPixel.h"
+#include "math.h"
 
-//Define os LED's
-#define vmc 7
-#define amr 6
-#define vdc 5
-#define vmp 4
-#define vdp 3
-#define buzzer A0
+#define PIN_WS2812B_1 32
+#define PIN_WS2812B_2 33
+#define NUM_PIXELS 64
+#define buzzer 16
 
-//Inicializa componentes
-LiquidCrystal lcd(8,9,10,11,12,13);
-Button botao(2);
+Adafruit_NeoPixel ws2812b_1(NUM_PIXELS, PIN_WS2812B_1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel ws2812b_2(NUM_PIXELS, PIN_WS2812B_2, NEO_GRB + NEO_KHZ800);
+LiquidCrystal_I2C lcd(0x27,16,2);
+Button botao(17);
 
-//Inicializa os estados
 State Fechado = State(fechado);
 State Amarelo = State(amarelo);
 State Aberto = State(aberto);
 State AbertoPCD = State(abertoPCD);
 
-FSM semaforo = FSM(Fechado); //Inicializa a máquina de estado no primeiro estado
+FSM semaforo = FSM(Fechado);
 
-//Inicializa os timers para controle do contador
 unsigned long millisInicial = 0;
 unsigned long millisAtual = 0;
 const long intervalo = 1000;
@@ -31,26 +29,24 @@ int t_amarelo = 5;
 int t_aberto = 10;
 int t_abertoPCD = 30;
 
-bool contando = true; //Flag para controle do timer
+bool contando = true;
 
 void setup() {
-  lcd.begin(16,2); //Instancia a tela
-  botao.begin(); //Instancia o botão
-  //Serial.begin(9600);
-
-  //Pinos 3 a 5 como OUTPUT
-  for(int i = 3; i < 8; i++)
-    pinMode(i, OUTPUT);
-
-  pinMode(buzzer, OUTPUT); //Buzzer como OUTPUT no pino A0
+  pinMode (PIN_WS2812B_1, OUTPUT);
+  pinMode (PIN_WS2812B_2, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  ws2812b_1.begin();
+  ws2812b_2.begin();
+  lcd.init();
+  botao.begin();
 }
 
 void loop() {
   //Transiciona o status e soma o tempo quando o botão for pressionado no status adequado
   if (botao.pressed() && semaforo.isInState(Aberto)){
-    t_abertoPCD = t_aberto + t_abertoPCD;
-    tone(buzzer,1500,250);
-    semaforo.immediateTransitionTo(AbertoPCD);
+    t_abertoPCD = t_aberto + t_abertoPCD; //Soma 30s ao timer atual
+    tone(buzzer,1500,250); //Reproduz um sinal sonoro distinto com o buzzer, para acusar o pressionamento do botão
+    semaforo.immediateTransitionTo(AbertoPCD); //Transição imediata para o estado 4
   }
 
   semaforo.update(); //Atualiza a máquina de estados
@@ -60,11 +56,8 @@ void fechado(){
   contando = true; //Ativa o contador regressivo
 
   //Seta os LED's
-  digitalWrite(vmc, LOW);
-  digitalWrite(amr, LOW);
-  digitalWrite(vdc, HIGH);
-  digitalWrite(vmp, HIGH);
-  digitalWrite(vdp, LOW);
+  verde_c();
+  vermelho_p();
 
   millisAtual = millis(); //Registra o momento em que estado foi carregado
 
@@ -72,7 +65,7 @@ void fechado(){
   if(millisAtual - millisInicial > intervalo && contando) {
     millisInicial = millis(); //Atualiza o timer inicial
     
-    //Prepara o display
+    //Exibe uma mensagem no display
     lcd.clear();
     lcd.print("Fechado pedestre");
     lcd.setCursor(0,1);
@@ -85,7 +78,7 @@ void fechado(){
       lcd.print("0" + String(t_fechado));
     }
 
-    tone(buzzer,500,100);
+    tone(buzzer,500,100); //Reproduz um sinal sonoro distinto através do buzzer
 
     t_fechado--; //Auto-decremento do timer
 
@@ -101,11 +94,7 @@ void fechado(){
 void amarelo(){
   contando = true;
 
-  digitalWrite(vmc, LOW);
-  digitalWrite(amr, HIGH);
-  digitalWrite(vdc, LOW);
-  digitalWrite(vmp, HIGH);
-  digitalWrite(vdp, LOW);
+  amarelo_c();
 
   millisAtual = millis();
 
@@ -138,11 +127,8 @@ void amarelo(){
 void aberto(){  
   contando = true;
 
-  digitalWrite(vmc, HIGH);
-  digitalWrite(amr, LOW);
-  digitalWrite(vdc, LOW);
-  digitalWrite(vmp, LOW);
-  digitalWrite(vdp, HIGH);
+  vermelho_c();
+  verde_p();
 
   millisAtual = millis();
 
@@ -163,6 +149,7 @@ void aberto(){
     tone(buzzer,1000,100);
 
     t_aberto--;
+
     if(t_aberto < 1){
       contando = false;
       t_aberto = 10;
@@ -175,11 +162,8 @@ void aberto(){
 void abertoPCD(){
   contando = true;
 
-  digitalWrite(vmc, HIGH);
-  digitalWrite(amr, LOW);
-  digitalWrite(vdc, LOW);
-  digitalWrite(vmp, LOW);
-  digitalWrite(vdp, HIGH);
+  vermelho_c();
+  verde_p();
 
   millisAtual = millis();
 
@@ -200,6 +184,7 @@ void abertoPCD(){
     tone(buzzer,1000,100);
     
     t_abertoPCD--;
+    
     if(t_abertoPCD < 1){
       contando = false;
       t_aberto = 10;
@@ -207,4 +192,57 @@ void abertoPCD(){
       semaforo.transitionTo(Fechado);
     }
   }
+}
+
+void vermelho_c(){
+  ws2812b_1.clear();
+
+  for (int pixel = 0; pixel < 65; pixel++) {
+    if(pixel != 2 && pixel != 3 && pixel != 4 && pixel != 5 && pixel != 11 && pixel != 12 && pixel != 16 && pixel != 23 && pixel != 24 && pixel != 25 && pixel != 30 && pixel != 31 && pixel != 32 && pixel != 33 && pixel != 38 && pixel != 39 && pixel != 40 && pixel != 47 && pixel != 51 && pixel != 52 && pixel != 58 && pixel != 59 && pixel != 60 && pixel != 61)
+      ws2812b_1.setPixelColor(pixel, ws2812b_1.Color(255, 0, 0));
+  }
+  ws2812b_1.show();
+}
+
+void amarelo_c(){
+  ws2812b_1.clear();
+
+  for (int pixel = 0; pixel < 65; pixel++) {
+    if(pixel != 3 && pixel != 4 && pixel != 11 && pixel != 12 && pixel != 19 && pixel != 20 && pixel != 27 && pixel != 28 && pixel != 35 && pixel != 36 && pixel != 51 && pixel != 52 && pixel != 59 && pixel != 60);
+    else
+      ws2812b_1.setPixelColor(pixel, ws2812b_1.Color(255, 255, 0));
+  }
+  ws2812b_1.show();
+}
+
+void verde_c(){
+  ws2812b_1.clear();
+
+  for (int pixel = 0; pixel < 65; pixel++) {
+    if(pixel != 3 && pixel != 4 && pixel != 10 && pixel != 11 && pixel != 12 && pixel != 13 && pixel != 17 && pixel != 18 && pixel != 21 && pixel != 22 && pixel != 24 && pixel != 25 && pixel != 30 && pixel != 31 && pixel != 32 && pixel != 35 && pixel != 36 && pixel != 39 && pixel != 42 && pixel != 43 && pixel != 44 && pixel != 45 && pixel != 49 && pixel != 50 && pixel != 53 && pixel != 54 && pixel != 56 && pixel != 57 && pixel != 62 && pixel != 63);
+    else
+      ws2812b_1.setPixelColor(pixel, ws2812b_1.Color(0, 255, 0));
+  }
+  ws2812b_1.show();
+}
+
+void vermelho_p(){
+  ws2812b_2.clear();
+
+  for (int pixel = 0; pixel < 65; pixel++) {
+    if(pixel != 2 && pixel != 3 && pixel != 4 && pixel != 5 && pixel != 11 && pixel != 12 && pixel != 16 && pixel != 23 && pixel != 24 && pixel != 25 && pixel != 30 && pixel != 31 && pixel != 32 && pixel != 33 && pixel != 38 && pixel != 39 && pixel != 40 && pixel != 47 && pixel != 51 && pixel != 52 && pixel != 58 && pixel != 59 && pixel != 60 && pixel != 61)
+      ws2812b_2.setPixelColor(pixel, ws2812b_2.Color(255, 0, 0));
+  }
+    ws2812b_2.show();
+}
+
+void verde_p(){
+  ws2812b_2.clear();
+
+  for (int pixel = 0; pixel < 65; pixel++) {
+    if(pixel != 3 && pixel != 4 && pixel != 10 && pixel != 11 && pixel != 12 && pixel != 13 && pixel != 17 && pixel != 18 && pixel != 21 && pixel != 22 && pixel != 24 && pixel != 25 && pixel != 30 && pixel != 31 && pixel != 32 && pixel != 35 && pixel != 36 && pixel != 39 && pixel != 42 && pixel != 43 && pixel != 44 && pixel != 45 && pixel != 49 && pixel != 50 && pixel != 53 && pixel != 54 && pixel != 56 && pixel != 57 && pixel != 62 && pixel != 63);
+    else
+      ws2812b_2.setPixelColor(pixel, ws2812b_2.Color(0, 255, 0));
+  }
+    ws2812b_2.show();
 }
